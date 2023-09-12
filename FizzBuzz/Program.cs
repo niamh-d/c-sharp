45bc490fe771cf026ec9017dc9664cef07ec038d
Niamh <85398097+niamh-d@users.noreply.github.com>
2023-09-12 22:18:14 +0300

    Add files via upload
    FizzBuzz in C#

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+for (int num = 1; num <= 100; num++)
+{
+    var output = new System.Text.StringBuilder("");
+    if (num % 3 == 0) output.Append("Fizz");
+    if (num % 5 == 0) output.Append("Buzz");
+    if (output.Length == 0) output.Append(num);
+    Console.WriteLine(output);
+}
